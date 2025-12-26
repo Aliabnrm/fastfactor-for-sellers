d7e83724fd47267865cc85d9761b7a3b01db1b83
Ali <aliabnar211@gmail.com>
2025-12-26 18:16:46 +0330

    feat: Integrate PWA support and update dependencies
    - Added `vite-plugin-pwa` to enable Progressive Web App features, enhancing offline capabilities and user experience.
    - Updated `package.json` to include the new PWA plugin.
    - Modified `vite.config.ts` to configure PWA settings, including service worker registration and caching strategies.
    - Updated `main.tsx` to register the service worker with appropriate callbacks for content updates and offline readiness.
    - Adjusted TypeScript definitions to support the new PWA registration module.

--- src/main.tsx
+++ src/main.tsx
@@ -2,5 +2,15 @@ import './index.css'
 import App from './App.tsx'
 import 'antd/dist/reset.css'
 import { createRoot } from 'react-dom/client'
+import { registerSW } from 'virtual:pwa-register'
+
+registerSW({
+  onNeedRefresh() {
+    console.log('New content available…')
+  },
+  onOfflineReady() {
+    console.log('App ready to work offline.')
+  },
+})
 
 createRoot(document.getElementById('root')!).render(<App />)
